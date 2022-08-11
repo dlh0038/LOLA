0b4cddf6fe1c1b89c6ff22111e0438bb802660b8
John Clay <jjclay@fbi.gov>
2022-08-11 12:40:03 -0400

    Started working on start lunch order page. - Added foreign key of restaurantid to order to link an order to specific restaurant - Added mudblazor components to page that allow order info to be input.

---
+++ Server/Migrations/20220811131306_restaurantInOrder.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LOLA.Server.Migrations
+{
+    public partial class restaurantInOrder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RestaurantId",
+                table: "Orders",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_RestaurantId",
+                table: "Orders",
+                column: "RestaurantId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Restaurants_RestaurantId",
+                table: "Orders",
+                column: "RestaurantId",
+                principalTable: "Restaurants",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Restaurants_RestaurantId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_RestaurantId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "RestaurantId",
+                table: "Orders");
+        }
+    }
+}
--- Shared/Models/Order.cs
+++ Shared/Models/Order.cs
@@ -6,4 +6,6 @@ public class Order
     public string OrderText {get; set;}
     public double OrderTotal {get; set;}
     public User User {get; set;} //this should be the foreign key
+
+    public Restaurant Restaurant {get; set;}
 }
